c0b5332b8f6ac577acc42511192655088b2e403b
hongmo sung <tesung30@naver.com>
2021-08-10 23:15:50 +0900

    Micrometer

---
+++ src/main/java/io/hongmo/toy/HelloController.java
@@ -0,0 +1,15 @@
+package io.hongmo.toy;
+
+import io.micrometer.core.annotation.Timed;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    @Timed(value = "hello", longTask = true)
+    public String hello() {
+        return "hello";
+    }
+}
