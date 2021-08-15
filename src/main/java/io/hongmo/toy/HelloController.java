95a610bfb6449635ef902df610ea9c4378c45c98
sunghongmo <hm.sung@softwareinlife.com>
2021-08-16 06:52:58 +0900

    log

--- src/main/java/io/hongmo/toy/HelloController.java
+++ src/main/java/io/hongmo/toy/HelloController.java
@@ -1,15 +1,18 @@
 package io.hongmo.toy;
 
 import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/api/hello")
     @Timed(value = "hello", longTask = true)
     public String hello() {
+        log.info("hello api");
         return "hello";
     }
 }
